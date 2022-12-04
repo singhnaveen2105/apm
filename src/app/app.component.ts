be56e95d1f444355802774d385b93dbac996c63a
naveen <singhnaveen2105@gmail.com>
2022-12-04 20:42:45 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+ // templateUrl: './app.component.html',
+  template: `
+  <div>
+    <h1>
+      <div>
+        {{pageTitle}}
+        <pm-products></pm-products>
+      </div>
+    </h1>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apm';
+  pageTitle: string ='Neo Product Management';
 }
